a8e5059ad0500d9320c8133dac41bf6c814ba589
EfeAdzhar <efeadzhar@gmail.com>
2022-04-19 21:38:33 +0200

    added coordinates and can also find by id

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var app = express();
 
+var coordinates = [
+    {
+    id: 1,
+    name: 'Station',
+    latitude: 52.251519,
+    longitude: 21.052143,
+    },
+    {
+    id: 2,
+    name: 'Home',
+    latitude: 52.25367,
+    longitude: 21.05233,
+    }
+]
+
 app.get('/', function(req, res) {
     res.send('Hello API')
+})
+
+app.get('/coordinates', function(req, res) {
+    res.send(coordinates)
+})
+
+app.get('/coordinates/:id', function(req, res) {
+    var coordinate = coordinates.find(function(coordinate) {
+        return coordinate.id === Number(req.params.id)
+    })
+    res.send(coordinate)
 })
 
 app.listen(3000, function () {
