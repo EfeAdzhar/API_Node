afa170ba89404cea6928b7ce219c9a253f330e03
EfeAdzhar <efeadzhar@gmail.com>
2022-04-29 20:52:18 +0200

    API for my diploma. Still in process. Hope it will be finished tommorow

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 var coordinates = [
     {
     id: 1,
     name: 'Station',
+    hours: '24/7',
     latitude: 52.251519,
     longitude: 21.052143,
     },
     {
     id: 2,
     name: 'Home',
+    hours: '7:00 - 23:00',
     latitude: 52.25367,
     longitude: 21.05233,
     }
@@ -28,6 +34,23 @@ app.get('/coordinates/:id', function(req, res) {
     var coordinate = coordinates.find(function(coordinate) {
         return coordinate.id === Number(req.params.id)
     })
+    res.send(coordinate)
+})
+
+app.post('/coordinates', function(req, res) {
+    var coordinate = {
+        id: Date.now(),
+        name: req.body.name
+    }
+    coordinates.push(coordinate)
+    res.send(coordinate)
+})
+
+app.put('/coordinates/:id', function (req, res) {
+    var coordinate = coordinates.find(function(coordinate) {
+        return coordinate.id === Number(req.params.id)
+    });
+    coordinate.name = req.body.name
     res.send(coordinate)
 })
 
