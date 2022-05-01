7262a7f5f3d1860f9e495036bcbd851582fc39e3
EfeAdzhar <efeadzhar@gmail.com>
2022-05-01 14:47:51 +0200

    trying

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient
+
 var app = express();
+var db; 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -39,11 +42,16 @@ app.get('/coordinates/:id', function(req, res) {
 
 app.post('/coordinates', function(req, res) {
     var coordinate = {
-        id: Date.now(),
         name: req.body.name
     }
-    coordinates.push(coordinate)
-    res.send(coordinate)
+    //res.send(coordinate)
+    db.collection('coordinates').insert(coordinate, function(err, result) {
+        if (err) {
+            console.log(err)
+            res.sendStatus(500)
+        }
+        res.send(coordinate)
+    })
 })
 
 app.put('/coordinates/:id', function (req, res) {
@@ -51,16 +59,22 @@ app.put('/coordinates/:id', function (req, res) {
         return coordinate.id === Number(req.params.id)
     });
     coordinate.name = req.body.name
-    res.send(coordinate)
+    res.sendStatus(200)
 })
 
 app.delete('/coordinates/:id', function (req, res) {
     coordinates = coordinates.filter(function(coordinate) {
         return coordinate.id !== Number(req.params.id)
     })
-    res.sendStatus(200);
+    res.sendStatus(200)
 })
 
-app.listen(3000, function () {
-    console.log('API app start')
+MongoClient.connect('mongodb://localhost:27017/myapi', function(err, database) {
+    if (err) {
+        return console.log(err)
+    }
+    db = database
+    app.listen(3000, function () {
+        console.log('API app start')
+    })
 })
